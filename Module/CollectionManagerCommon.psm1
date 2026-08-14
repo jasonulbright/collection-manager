94f93e98c0762b439896bebbce90b95a087133d2
Jason Ulbright <jason.ulbright@gmail.com>
2026-08-14 01:33:18 -0400

    HtmlEncode site-derived text in the HTML exporter

--- Module/CollectionManagerCommon.psm1
+++ Module/CollectionManagerCommon.psm1
@@ -696,21 +696,23 @@ function Export-CollectionHtml {
         '</style>'
     ) -join "`r`n"
 
-    $headerRow = ($DataTable.Columns | ForEach-Object { "<th>$($_.ColumnName)</th>" }) -join ''
+    # HtmlEncode every site-derived string: names with angle brackets or
+    # ampersands otherwise corrupt the report markup.
+    $headerRow = ($DataTable.Columns | ForEach-Object { "<th>$([System.Net.WebUtility]::HtmlEncode($_.ColumnName))</th>" }) -join ''
     $bodyRows = foreach ($row in $DataTable.Rows) {
         $cells = foreach ($col in $DataTable.Columns) {
             $val = [string]$row[$col.ColumnName]
-            "<td>$val</td>"
+            "<td>$([System.Net.WebUtility]::HtmlEncode($val))</td>"
         }
         "<tr>$($cells -join '')</tr>"
     }
 
     $html = @(
         '<!DOCTYPE html>',
-        '<html><head><meta charset="utf-8"><title>' + $ReportTitle + '</title>',
+        '<html><head><meta charset="utf-8"><title>' + [System.Net.WebUtility]::HtmlEncode($ReportTitle) + '</title>',
         $css,
         '</head><body>',
-        "<h1>$ReportTitle</h1>",
+        "<h1>$([System.Net.WebUtility]::HtmlEncode($ReportTitle))</h1>",
         "<div class='summary'>Generated: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') | Rows: $($DataTable.Rows.Count)</div>",
         "<table><thead><tr>$headerRow</tr></thead>",
         "<tbody>$($bodyRows -join "`r`n")</tbody></table>",
